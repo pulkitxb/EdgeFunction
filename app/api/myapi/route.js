72a4f53fb1072a0117e6c8fb9188928a2646a96d
Pulkit Joshi <pulkit.b.joshi@accenture.com>
2025-04-22 16:32:07 +0530

    edge function changes

--- app/api/myapi/route.js
+++ app/api/myapi/route.js
@@ -1,15 +1,9 @@
-const { NextResponse } = require("next/server")
-
-// export const config = {
-//     runtime: 'edge'
-// }
+import { NextResponse } from "next/server";
 
 export const runtime = "edge";
 
-const handler = (req) => {
-    return NextResponse.json({
-        name: `Hello from ${req.url} I'm now an edge function!`
-    })
+export async function GET(req) {
+  return NextResponse.json({
+    name: `Hello from ${req.url}, I'm now an edge function!`
+  });
 }
-
-export default handler;
